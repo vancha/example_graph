f747264e7492fa69bad7e4518ed465b21ca35d98
Tjipke van der Heide <tjipkevdh@gmail.com>
2021-12-28 10:55:01 +0100

    Update main.rs
    IT WORKS!!1!!

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,19 @@ struct Graph<T> {
     ///adjacent generic values
     adjacency_list: HashMap<T, HashSet<T>>,
 }
+trait IsSmallCity {
+    fn is_small_city(&self)->bool;
+}
+impl<T: std::fmt::Display> IsSmallCity for T {
+    fn is_small_city(&self)->bool {
+        self.to_string().chars().filter(|c| c.is_ascii_uppercase()).count() == 0
+    }
+}
+
 ///the generic objects used by the graph must implement hash, display, eq and copy
 impl<T> Graph<T>
 where
-    T: std::hash::Hash + Eq + std::fmt::Display + Copy,
+    T: std::hash::Hash + Eq + std::fmt::Display + std::fmt::Debug + Copy + ToString,
 {
     ///builder pattern: easily instantiate a new graph
     fn new() -> Self {
@@ -18,11 +27,37 @@ where
         }
     }
     ///add directed edge, goes one way
-    fn add_edge(&mut self, from_vertex: T, to_vertex: T) {
+    fn add_directed_edge(&mut self, from_vertex: T, to_vertex: T) {
         if self.adjacency_list.contains_key(&from_vertex) {
-            self.adjacency_list.get_mut(&from_vertex).unwrap().insert(to_vertex);
+            self.adjacency_list
+                .get_mut(&from_vertex)
+                .unwrap()
+                .insert(to_vertex);
         } else {
-            self.adjacency_list.insert(from_vertex, HashSet::from([to_vertex]));
+            self.adjacency_list
+                .insert(from_vertex, HashSet::from([to_vertex]));
+        }
+    }
+
+    ///add undirected edge, goes two ways
+    fn add_undirected_edge(&mut self, from_vertex: T, to_vertex: T) {
+        if self.adjacency_list.contains_key(&from_vertex) {
+            self.adjacency_list
+                .get_mut(&from_vertex)
+                .unwrap()
+                .insert(to_vertex);
+        } else {
+            self.adjacency_list
+                .insert(from_vertex, HashSet::from([to_vertex]));
+        }
+        if self.adjacency_list.contains_key(&to_vertex) {
+            self.adjacency_list
+                .get_mut(&to_vertex)
+                .unwrap()
+                .insert(from_vertex);
+        } else {
+            self.adjacency_list
+                .insert(to_vertex, HashSet::from([from_vertex]));
         }
     }
     ///get neighbours for any vertex
@@ -66,34 +101,71 @@ where
             }
         }
     }
+
     ///count all paths between two vertices in the graph
-    fn count_paths_between(&self, starting_node: T, ending_node: T, current: T, visited: Vec<T>)-> i32 {
-        let mut stack: Vec<T> = Vec::from([starting_node]);
-        while !stack.is_empty() {
-            let current = stack.pop().unwrap();
-            println!("{}", current);
-            if let Some(neighbours) = self.get_neighbours(current) {
-                for neighbour in self.get_neighbours(current).unwrap() {
-                    stack.push(neighbour);
+    fn count_paths_between(
+        &self,
+        u: T,
+        d: T,
+        beingVisited: &mut HashMap<T, bool>,
+        path: &mut Vec<T>,
+        path_count: &mut i32,
+    ) {
+        beingVisited.insert(u, true);
+        path.push(u);
+        if u == d {
+            *path_count += 1;
+            println!("current path: {:?}", path);
+        } else {
+            if let Some(neighbours) = self.get_neighbours(u) {
+                for i in neighbours {
+                    if !beingVisited.contains_key(&i) || !i.is_small_city() {
+                        self.count_paths_between(i, d, beingVisited, path,path_count);
+                    }
                 }
             }
         }
-    6
+        path.pop();
+        beingVisited.remove_entry(&u);
+    }
+
+    ///count all paths between two vertices in the graph
+    fn count_paths_between_caves(
+        &self,
+        u: T,
+        d: T,
+        beingVisited: &mut HashMap<T, bool>,
+        path: &mut Vec<T>,
+        path_count: &mut i32,
+    ) {
+        beingVisited.insert(u, true);
+        path.push(u);
+        if u == d {
+            *path_count += 1;
+            println!("current path: {:?}", path);
+        } else {
+            if let Some(neighbours) = self.get_neighbours(u) {
+                for i in neighbours {
+                    if !beingVisited.contains_key(&i)  || !i.is_small_city() {
+                        self.count_paths_between_caves(i, d, beingVisited, path,path_count);
+                    }
+                }
+            }
+        }
+        path.pop();
+        beingVisited.remove_entry(&u);
     }
 }
 
 fn main() {
-    let mut g:Graph<&str> = Graph::new();
-    g.add_edge("start", "A");
-    g.add_edge("start", "b");
-    g.add_edge("A", "c");
-    g.add_edge("A", "b");
-    g.add_edge("b", "d");
-    g.add_edge("b", "d");
-    g.add_edge("A", "end");
-    g.add_edge("b", "end");
-    g.print_depth_first("start");
-    println!("\n");
-    g.print_breadth_first("start");
-    println!("nr of paths: {}",g.count_paths_between("start","end","start",vec![]))
+    let mut g: Graph<&str> = Graph::new();
+    g.add_undirected_edge("start", "A");
+    g.add_undirected_edge("start", "b");
+    g.add_undirected_edge("A", "c");
+    g.add_undirected_edge("A", "b");
+    g.add_undirected_edge("b", "d");
+    g.add_undirected_edge("A", "end");
+    g.add_undirected_edge("b", "end");
+    let mut total_paths = 0;
+    g.count_paths_between_caves("start","end",&mut HashMap::new(), &mut vec![], &mut total_paths);
 }
